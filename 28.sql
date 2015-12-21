d5b1948162f9acd7fd42db49fc0212330bfed5f7
Francisco Jarana Camacho <franciscojarana92@gmail.com>
2015-12-21 10:45:49 +0100

    Ejercicio 28

--- 28.sql
+++ 28.sql
@@ -5,15 +5,12 @@ cursor c_objetos is select table_name
                     from dba_tab_privs
                     where grantee = p_usuario or owner = p_usuario;
 
-v_objeto dba_tab_privs.table_name%type;
-
 begin
 
 dbms_output.put_line('Estos son los objetos a los que tiene acceso el usuario '||p_usuario||':');
 
 for a in c_objetos loop
-	fetch c_objetos into v_objeto;
-	dbms_output.put_line(v_objeto);
+	dbms_output.put_line(a.table_name);
 end loop;
 
 end;
